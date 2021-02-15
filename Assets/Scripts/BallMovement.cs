2e3955a7e1cea0e2fa9f37f745bf04827aaa2935
Dalasu2 <Dalasu2@uic.edu>
2021-02-15 15:01:39 -0600

    Added functionality to pick up ball

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour {
     public float kickForce = 10;
     public float rotationSpeed = 90;
-    Rigidbody rb;
-    Transform t;
+    public GameObject player;
+    public Transform body;
+    private Rigidbody rb;
+    private SphereCollider sc;
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody>();
+        sc = GetComponent<SphereCollider>();
     } // End of Start
 
     private void OnTriggerStay(Collider other) {
@@ -17,5 +20,23 @@ public class BallMovement : MonoBehaviour {
         if(other.gameObject.name == "football-player" && other.transform.GetComponent<PlayerMovement>().isShooting) {
             rb.velocity += ((other.transform.forward * kickForce) + (new Vector3(0, 2, 0))); // Update velocity
         }
-    } // End of OnCollisionEnter
+    } // End of OnTriggerStay
+
+    // Code added to allow player to change position of ball
+    private void OnMouseDown() {
+        sc.enabled = false;
+        rb.useGravity = false;
+        rb.isKinematic = true;
+
+        this.transform.position = body.position;
+        this.transform.parent = player.transform;
+    } // End of OnMouseDown
+
+    private void OnMouseUp() {
+        this.transform.parent = null;
+        rb.useGravity = true;
+        rb.isKinematic = false;
+        sc.enabled = true;
+    } // End of OnMouseUp
+    // End of code added
 }
